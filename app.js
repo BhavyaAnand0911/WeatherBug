68d641a4aaa2cff6cdd25c3e8cf397809c31e471
Bhavya-Anand <bhavyaanand0911@gmail.com>
2023-04-10 23:47:26 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,132 @@
+const express=require('express');
+const app=express();
+const PORT=5556;
+const http = require("http");
+const bodyParser = require("body-parser");
+const request=require("request");
+const ejs=require('ejs');
+const fetch = require('node-fetch');
+
+app.get('/index3', (req, res) => {
+  const city = req.query.city;
+  const apiKey = 'a298ffc6780395b462954bb9a478d046'; // replace with your actual API key
+  // build the API URL with the city and API key
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const temperature = data.main.temp;
+      const windSpeed = data.wind.speed;
+      const aqi = data.main.aqi;
+      const weatherData = {temp: temperature, windSpeed: windSpeed, aqi: aqi};
+
+      // return the weather data as JSON
+      res.json(weatherData);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error retrieving weather data');
+    });
+});
+
+
+//LOGIN APP
+//Login using mongoDB
+
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+// Use cookie parser middleware
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use JSON middleware for parsing request body
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/WeatherBug', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Serve login page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+// Login route
+app.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find user by email
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    // User not found
+    res.status(401).send('Invalid email or password');
+  } else {
+    if (user.password !== password) {
+      // Password incorrect
+      res.status(401).send('Invalid email or password');
+    } else {
+      // Set user ID cookie and redirect to dashboard
+      res.cookie('user_id', user._id.toString(), { maxAge: 3600000 }); // Cookie expires after 1 hour
+      res.redirect('/home.html');
+    }
+  }
+});
+
+app.get('/signup.html', (req,res)=>{
+  res.sendFile(__dirname + '/views/signup.html');
+});
+
+app.post('/signup.html', async (req, res) => {
+  const { email, password } = req.body;
+  // console.log({ email, password });
+  const user = new User({ email, password });
+
+  try {
+    await user.save();
+    res.redirect(302, '/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error signing up user');
+  }
+});
+
+app.get('/home.html', (req, res) => {
+  if (req.cookies.user_id) {
+    res.sendFile(__dirname + '/views/home.html');
+  } else {
+    res.status(401).send('<h1>Unauthorized User</h1>');
+  }
+});
+
+//Routing Pages
+app.get("/contact.html",(req,res)=>{
+  res.sendFile(__dirname+'/views/contact.html');
+})
+
+app.get("/index3.ejs",(req,res)=>{
+  res.render("index3.ejs")
+})
+
+app.use(express.static('public'));
+app.use('./Styles',express.static(__dirname+'/public/Styles'));
+app.use('./Scripts',express.static(__dirname+'/public/Scripts'));
+
+
+app.listen(PORT,()=>{
+  console.log('Listening');
+});
